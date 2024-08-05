51aac2adb0335725cfffebbde8f38877764468a6
Reino Powell <rpowell@retrorabbit.co.za>
2024-08-05 17:41:18 +0200

    Added javascript scroll function

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,22 @@
-//Javascript file for functionality
+//==========================Javascript file for functionality==========================
+
+//==========================Back to top scroll functionality==========================
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    const button = document.getElementById("back-to-top");
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        button.style.display = "block";
+    } else {
+        button.style.display = "none";
+    }
+}
+
+function scrollToTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+}
 
 
-
+//==========================function==========================
 
